3e27df342c2287567083351d25a105e24e30da38
Marc Löhe <marc@marcloehe.de>
2017-03-08 08:16:56 +0100

    Test update on project two

--- packages/project-two/index.js
+++ packages/project-two/index.js
@@ -1,6 +1,7 @@
 let dependencyOne = require('dependency-one');
 let dependencyThree = require('dependency-three');
 
-console.log('project two');
+console.log('project two start');
 dependencyOne();
 dependencyThree();
+console.log('project two end');
